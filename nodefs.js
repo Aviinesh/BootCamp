f84f73d95927e2c8d1ca489468e6d58902f4e2cc
Aviinesh <aviineshks@gmail.com>
2021-08-27 22:06:25 +0530

    added node learning file

---
+++ nodefs.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+
+console.log(1);
+fs.readFile("sample.txt",function(err,data){ //error first callback operation, if file cannot be found while reading, it will show error first//
+console.log("error is ....."+err);
+console.log("Data is ....."+data);
+});
+console.log(2);
+
+//12Hiiii
