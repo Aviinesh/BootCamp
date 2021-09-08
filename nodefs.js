3ab2f4fff66bfb433ad8cb2771138bc256c8042e
Aviinesh <aviineshks@gmail.com>
2021-09-08 14:00:24 +0530

    added files with some changes

--- nodefs.js
+++ nodefs.js
@@ -10,10 +10,11 @@ var fs = require("fs");
 console.log(1);
 
 //Asychronous functions needs callbacks
+//Sychnronous method to perform step by step functions
 var result = fs.readFileSync("sample.txt");
 console.log(result.toString());
 
-console.log(2); //1Hiii2
+console.log(2); //first output 1 then Hiii & then output 2
 
 
 
--- nodehttp.js
+++ nodehttp.js
@@ -1,19 +1,19 @@
 var http = require("http");
 var fs = require("fs");
 
-//port and //host
-
+//port and //host - host can be any ip address or your local computer
 //by default we use "localhost" to refer to our computer, 127.0.0.1
 
 var port = 3000;
 var host ="127.0.0.1";
 
 //create server and serve internal html
-
+// when working with server in http, we use a function with 2 parameters - request & response
+//to read from external file 'index.html'
 var server = http.createServer(function(req,res){
-    res.statusCode =200;
-    res.setHeader('Content-Type','text/html');
-    fs.readFile("index.html",function(err,data){
+    res.statusCode =200; //whenever you hit a webpage, you get a status code, 200 means success, 400-error, 404-website not available, 401-unauthorized access
+    res.setHeader('Content-Type','text/html'); //mentioning what type of file
+    fs.readFile("index.html",function(err,data){  
         if(!err){
             res.write(data);
             res.end();
@@ -24,3 +24,12 @@ var server = http.createServer(function(req,res){
 server.listen(port,host,function(){
     console.log("server started successfully");
 })
+
+//to show a direct output
+//var server = http.createServer(function(req,res){
+//  res.end("Hello World");
+// });
+
+//server.listen(port,host,function){
+//  console.log("server started succesfully");
+// });
